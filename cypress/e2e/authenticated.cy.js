7cb8fc8ee40f7fee133f3d12c37510bf0ed1c458
Ed Carlos da Silva <edcarlos20@hotmail.com>
2023-11-24 19:06:12 -0300

    Configure cypress cloud

--- cypress/e2e/authenticated.cy.js
+++ cypress/e2e/authenticated.cy.js
@@ -34,15 +34,14 @@ describe('Scenarios where authentication is a pre-condition', () => {
       .its('state')
       .should('be.equal', 'Complete')
   })
-  
   it('logs out', () => {
     cy.visit('/')
     cy.wait('@getNotes')
     
     if (Cypress.config('viewportWidth') < Cypress.env('viewportWidthBreakpoint')) {
-      cy.get('.navbar-toggle.collapsed')
-        .should('be.visible')
-        .click()
+    cy.get('.navbar-toggle.collapsed')
+    .should('be.visible')
+    .click()
     }
 
     cy.contains('.nav a', 'Logout').click()
