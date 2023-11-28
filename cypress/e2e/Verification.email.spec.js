589e155c86cdadeb41eb56d3781404e984adf9f8
Ed Carlos da Silva <edcarlos20@hotmail.com>
2023-11-28 16:17:33 -0300

    Configure cypress cloud

--- cypress/e2e/Verification.email.spec.js
+++ cypress/e2e/Verification.email.spec.js
@@ -1,8 +1,8 @@
-const serverId = 'l2xuyzoo';
+const serverId = 'l2xuyzoo'
 const testEmail = '91eba393-1636-4ac2-b77b-49f217e5c428@l2xuyzoo.mailosaur.net'
 
 cy.mailosaurGetMessage(serverId, {
   sentTo: testEmail
 }).then(email => {
-  expect(email.subject).to.equal('Your verification code');
+  expect(email.subject).to.equal('Your verification code')
 })
